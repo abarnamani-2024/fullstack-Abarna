3bdf129fcbd3e559dc7d49014b34a3aa7c1b6f88
Abarnamani-2024 <abarnamani1127@gmail.com>
2024-10-15 12:34:09 +0530

    all

---
+++ demojs.js
@@ -0,0 +1,13 @@
+console.log("welcome");
+n=1;
+switch (n)
+{
+case 1:
+console.log("Verify");
+break;
+case 2:
+    console.log("check");
+    break;
+    case 3:
+        console.log("submit");
+}
